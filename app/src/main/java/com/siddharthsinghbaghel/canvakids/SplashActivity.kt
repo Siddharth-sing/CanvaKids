41ac42a90c4c8e58924861de3a66891d4d88036e
Siddharth Singh Baghel <siddharthsinghrewa5@gmail.com>
2021-05-13 23:01:38 +0530

    Splash Screen Added

---
+++ app/src/main/java/com/siddharthsinghbaghel/canvakids/SplashActivity.kt
@@ -0,0 +1,24 @@
+package com.siddharthsinghbaghel.canvakids
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 8000)
+
+    }
+}
+
+
+
